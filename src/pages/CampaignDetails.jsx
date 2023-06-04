93a22f57e411b314a9eda3204f6ff66185788de6
Divyesh Vankar <divyeshvankar.iitg@gmail.com>
2023-06-04 16:37:59 +0530

    complete the create campaign and dashboard

---
+++ src/pages/CampaignDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CampaignDetails = () => {
+  return (
+    <div>
+      CampignDetails
+    </div>
+  )
+}
+
+export default CampaignDetails
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily :{
+        epilogue :
+        ['Epilogue',
+        'sans-serif'
+
+        ],
+      },
+      boxShadow :{
+        secondory : '10px 10px 20px rgba(2,2,2,0.25)',
+      },
+    },
   },
   plugins: [],
 }
